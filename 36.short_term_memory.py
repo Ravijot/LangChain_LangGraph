3b4e2b9b0cb337de9ed487b52f280127464396a3
unknown <ravijot1212@gmail.com>
2025-07-05 13:14:29 +0530

    langgraph

---
+++ 36.short_term_memory.py
@@ -0,0 +1,42 @@
+from langchain_core.messages import BaseMessage
+from langgraph.graph import add_messages
+from langgraph.func import entrypoint, task
+from langgraph.checkpoint.memory import MemorySaver
+import os 
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+
+try: 
+    load_dotenv()
+    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    print("Environment variables loaded successfully.")
+except Exception as e:
+    print(f"Error loading environment variables: {e}")
+    
+model = init_chat_model("gpt-4o-mini", model_provider="openai", temperature=0.8)
+
+
+
+@task
+def call_model(messages: list[BaseMessage]):
+    response = model.invoke(messages)
+    return response
+
+checkpointer = MemorySaver()
+
+@entrypoint(checkpointer=checkpointer)
+def workflow(inputs: list[BaseMessage], *, previous: list[BaseMessage]):
+    if previous:
+        inputs = add_messages(previous, inputs)
+
+    response = call_model(inputs).result()
+    return entrypoint.final(value=response, save=add_messages(inputs, response))
+
+config = {"configurable": {"thread_id": "1"}}
+input_message = {"role": "user", "content": "hi! I'm bob"}
+for chunk in workflow.stream([input_message], config, stream_mode="values"):
+    chunk.pretty_print()
+
+input_message = {"role": "user", "content": "what's my name?"}
+for chunk in workflow.stream([input_message], config, stream_mode="values"):
+    chunk.pretty_print()
