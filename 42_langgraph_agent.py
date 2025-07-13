0fff59cf3a1c289a6b873800054a208fcfccee59
unknown <ravijot1212@gmail.com>
2025-07-13 22:01:02 +0530

    langgraph

--- 42_langgraph_agent.py
+++ 42_langgraph_agent.py
@@ -7,6 +7,7 @@ from langchain_core.tools import StructuredTool, tool
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import InMemorySaver
 
+
 try: 
     load_dotenv()
     os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
@@ -17,6 +18,11 @@ except Exception as e:
 model = init_chat_model("gpt-4o-mini", model_provider="openai", temperature=0.8)
 
 api_key = os.getenv('weather_api_key')
+
+class WeatherResponse(BaseModel):
+    temperature : str = Field(description="Current temperature in the city")
+    weather_details : str = Field(description="Give all weather details in the city")
+
 
 class Input(BaseModel):
     city: str = Field(description="Name of the city which data need to be retrieved")
@@ -59,7 +65,8 @@ agent = create_react_agent(
     model=model,
     tools=Tools().get_tools(),
     checkpointer=checkpointer,
-    prompt="You are a helpful assistant"  
+    prompt="You are a helpful weather assistant" ,
+    response_format=WeatherResponse
 )
 
 # Run the agent
@@ -69,4 +76,5 @@ agent_response = agent.invoke(
     {"messages": [{"role": "user", "content": "what about new york?"}]},
     config
 )
-print(agent_response['messages'][-1].content)
+# print(agent_response['messages'][-1].content)
+print(agent_response["structured_response"])
